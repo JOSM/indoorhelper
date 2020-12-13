14de212d56a057b2bed566710f9c9e1e67bf5d63
rebeccasc <65017227+rebeccasc@users.noreply.github.com>
2020-12-13 14:09:38 +0100

    Add ability to configure the file optimization

--- src/io/parser/utils/optimizer/FileOptimizer.java
+++ src/io/parser/utils/optimizer/FileOptimizer.java
@@ -15,33 +15,34 @@ import java.util.Scanner;
 public class FileOptimizer {
 
     /**
-     * Optimizes an ifc file. Removes block comments.
+     * Optimizes an ifc file following the given config parameters.
      *
      * @param ifcFilepath to file to optimize
      * @return optimized file as {@link File}
      * @throws IOException
      */
-    public static File optimizeIfcFile(String ifcFilepath) throws IOException {
+    public static File optimizeIfcFile(Configuration config, String ifcFilepath) throws IOException {
         // TODO Maybe find a better way than creating a temporary file...
         File tempFile = File.createTempFile(getFileName(ifcFilepath) + "_optimized", ".ifc");
         tempFile.deleteOnExit();
 
-        StringBuilder optimizedInput = new StringBuilder("");
-        try {
-            File file = new File(ifcFilepath);
-            Scanner reader = new Scanner(file, StandardCharsets.UTF_8.name());
-            while (reader.hasNextLine()) {
-                optimizedInput.append(reader.nextLine().replaceAll("/\\*.*?\\*/", "")).append("\n");
+        if(config.REMOVE_BLOCK_COMMENTS){
+            StringBuilder optimizedInput = new StringBuilder("");
+            try {
+                File file = new File(ifcFilepath);
+                Scanner reader = new Scanner(file, StandardCharsets.UTF_8.name());
+                while (reader.hasNextLine()) {
+                    optimizedInput.append(reader.nextLine().replaceAll("/\\*.*?\\*/", "")).append("\n");
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                Logging.error(e.getMessage());
             }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            Logging.error(e.getMessage());
-        }
 
-        try (FileWriter writer = new FileWriter(tempFile)) {
-            writer.write(String.valueOf(optimizedInput));
+            try (FileWriter writer = new FileWriter(tempFile)) {
+                writer.write(String.valueOf(optimizedInput));
+            }
         }
-
         return tempFile;
     }
 
@@ -57,6 +58,17 @@ public class FileOptimizer {
             return parts[parts.length - 1].split("\\.")[0];
         } catch (NullPointerException e) {
             return "indoorhelper_ifc_optimized";
+        }
+    }
+
+    /**
+     * Configuration class for file optimization tasks
+     */
+    public static class Configuration {
+        public final boolean REMOVE_BLOCK_COMMENTS;
+
+        public Configuration(boolean removeBlockCommands) {
+            REMOVE_BLOCK_COMMENTS = removeBlockCommands;
         }
     }
 
